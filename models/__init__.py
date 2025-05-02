5fb1bcadfbfd71164c82a2e3898d53d25554453a
amirhnazerii <anazeri@clemson.edu>
2025-05-02 17:56:14 -0400

    added: [all DETR set-up files]

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 from .detr import build
-from .detr_robust import build_robust
+from .detr_robust import build as build_robust
 
 def build_model(args):
     return build_robust(args) if args.robust else build(args) 
